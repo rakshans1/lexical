def5548613549b7a883fb281f2a7f5f0d56d9d74
Rakshan Shetty <shetty.raxx555@gmail.com>
2024-01-16 12:42:41 +0530

    update lexical

--- install.sh
+++ install.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-nix profile install github:lexical-lsp/lexical/557847d4cf11863731a6da84987413136a0c2a74
+nix profile install github:lexical-lsp/lexical/2b266ea7ff077cad578c73ee75d79c310a406864
