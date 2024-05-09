026c6840d73072d1828d326772fbc588343ad20a
Rakshan Shetty <shetty.raxx555@gmail.com>
2024-05-09 17:25:23 +0530

    update

--- install.sh
+++ install.sh
@@ -3,4 +3,4 @@
 index=$(nix profile list  | grep lexical | awk '{print $1}')
 nix profile remove $index
 
-nix profile install github:lexical-lsp/lexical/2c49cd5d5609606c57a3f8eb11776e295b29f3d8
+nix profile install github:lexical-lsp/lexical/16091d0722ae46703eb351d426c426f8ece4d1e7
